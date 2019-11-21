5f6cdb36b22259d75565af88ac189c369ab30934
Aan9222 <170709222@students.uajy.ac.id>
2019-11-21 17:17:02 +0700

    my first commit

---
+++ precache-manifest.64a421f8d46e5b1a6cdd62c27a9faaf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9217139145efa5a516",
+    "url": "/css/chunk-vendors.450cd38a.css"
+  },
+  {
+    "revision": "4a1a64c19afa2d26dd3ae2afb70af3b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c67eaeec9f8aa7313ce",
+    "url": "/js/app.770972b4.js"
+  },
+  {
+    "revision": "6c9217139145efa5a516",
+    "url": "/js/chunk-vendors.ef16937d.js"
+  },
+  {
+    "revision": "2463affc88074d89f7e318861801d666",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
